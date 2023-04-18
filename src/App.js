e2cff019eeaff2b3f2343359a0117af5e9ee24b5
Joulence <4erviken@gmail.com>
2023-04-18 17:04:00 +0300

    Managing a List Of Users via State

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AddUser from './components/Users/AddUser';
 import UserList from './components/Users/UserList';
 
 
 function App() {
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age: uAge, id: (userList.length + 1).toString() }];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
-      <UserList users={[]}/>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 }
--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,13 +1,13 @@
 import Card from "../UI/Card";
 
-import classes from "./AddUser.module.css";
+import classes from "./UserList.module.css";
 
 const UserList = (props) => {
     return (
         <Card className={classes.users}>
             <ul>
                 {props.users.map((user) => (
-                    <li>
+                    <li key={user.id}>
                         {user.name} ({user.age} years old)
                     </li>))}
             </ul>
